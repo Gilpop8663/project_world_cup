3f662cfbf7f344b557463c8782820461937c6ae0
양성호 <sungho@yangseonghoui-MacBookAir.local>
2022-03-05 15:56:14 +0900

    Add : 메인페이지 틀 구성 및 라우팅 완료

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,13 +2,36 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
-const WorldCupTitle = styled(Link)`
+const WorldCupTitle = styled.p`
   font-size: 30px;
-  /* text-decoration: none; */
 `;
 
+const Container = styled.div`
+  display: grid;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 40px;
+  margin: 0 auto;
+  width: 70%;
+`;
+const TitleBox: any = styled.div`
+  background-color: beige;
+  border-radius: 20px;
+  width: 100%;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+// const Container = styled.div``;
+// const Container = styled.div``;
+
 export default function Home() {
+  const navigate = useNavigate();
+
   const [listArr, setListArr] = useState<any>([{}]);
   useEffect(() => {
     axios.get('http://localhost:4000/world').then((res) => {
@@ -16,13 +39,24 @@ export default function Home() {
       setListArr(res.data);
     });
   }, []);
+
+  const goToWorldCup = (item: any) => {
+    navigate(`/world/${item.id}`);
+  };
+
   return (
-    <div>
+    <Container>
       {listArr.map((item: any, idx: number) => (
         <div key={idx}>
-          <WorldCupTitle to={`/world/${item.id}`}>{item.title}</WorldCupTitle>
+          <TitleBox
+            onClick={() => {
+              goToWorldCup(item);
+            }}
+          >
+            <WorldCupTitle>{item.title}</WorldCupTitle>
+          </TitleBox>
         </div>
       ))}
-    </div>
+    </Container>
   );
 }
