2079a0885361265e380144b10a82299aaf22927b
gilpop8663 <wolfye@naver.com>
2025-02-27 02:02:21 +0900

    chore: BASE_URL 변경

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -23,8 +23,7 @@ export const ERROR_MIN = '1글자 이상 입력해주세요';
 export const ERROR_MAX = '10글자 이하로 입력해주세요';
 export const ERROR_REQUIRED = '필수로 입력해야 합니다';
 
-export const BASE_URL =
-  'https://portfolio.coddink.com/api/fakeProjects';
+export const BASE_URL = 'https://portfolio.coddink.com/api/fakeProjects';
 
 export const RIGHT_SELECT = 'right';
 export const LEFT_SELECT = 'left';
